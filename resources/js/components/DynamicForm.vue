0249169f79be00b82b2c4e272f8ea2af90ca8149
themadamin <madaminme@gmail.com>
2024-09-23 12:11:15 +0500

    nested set front-end implemented

--- resources/js/components/DynamicForm.vue
+++ resources/js/components/DynamicForm.vue
@@ -2,11 +2,14 @@
         <Form class="d-flex flex-column" @submit="handleSubmit">
             <div v-for="{ as, name, label, ...attrs } in schema.fields" :key="name" class="row mb-4 pb-2">
                 <label :for="name" class="form-label">{{label}}</label>
-                <Field :as="as" :id="name" :name="name" v-bind="attrs" class="form-control form-control-lg">
-                    <option v-if="as === 'select'" v-for="option in attrs.options" :key="option.value" :value="option.id">{{ option.name }}</option>
+                <Field :as="as"
+                       :id="name" :name="name"
+                       v-bind="attrs" class="form-control form-control-lg">
+                    <slot name="selection"/>
                 </Field>
                 <ErrorMessage :name="name" class="text-danger"/>
             </div>
+            <slot name="selection"/>
             <div class="mt-2 d-flex justify-content-between">
                 <slot name="delete"/>
                 <button class="btn btn-primary">{{this.submitText}}</button>
